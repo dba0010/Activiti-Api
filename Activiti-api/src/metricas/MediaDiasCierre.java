a59ee262f9a94a3d489fc6bf3cce94908afab571
dba0010 <dba0010@alu.ubu.es>
2016-01-17 16:12:43 +0100

    Resuelto error métrica MediaDiasCierre
    Error producido al eliminar código duplicado.
    Eliminadas versiones anteriores de la memoria en formato odt.

--- Activiti-api/src/metricas/MediaDiasCierre.java
+++ Activiti-api/src/metricas/MediaDiasCierre.java
@@ -3,6 +3,8 @@ package metricas;
 import java.io.IOException;
 import java.util.List;
 
+import org.eclipse.egit.github.core.Issue;
+
 import motorMetricas.Descripcion;
 import motorMetricas.Metrica;
 import motorMetricas.Valor;
@@ -20,7 +22,15 @@ public class MediaDiasCierre extends Metrica
 	public Valor run(List<?> lista) throws IOException
 	{		
 		double mediaDias = 0;
-		int cerradas = obternerIssuesCerradasGitHub(lista).getValor();
+		int cerradas = 0;
+		for(Object x : lista)
+  		{
+  			if(((Issue) x).getState().equals("closed"))
+ 			{
+ 				cerradas++;
+ 				mediaDias += (((Issue) x).getClosedAt().getTime() - ((Issue) x).getCreatedAt().getTime() )/ (1000 * 60 * 60 * 24);
+ 			}
+ 		}
 		if(cerradas == 0){cerradas = 1;}
 		mediaDias /= cerradas;
 		
